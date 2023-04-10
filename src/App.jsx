e08924e348afcdcf499da7e640322483be62c966
Oyebamiji Alex <alexferanmi390@gmail.com>
2023-04-10 17:25:51 +0100

    omo!

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import NoMobile from "./components/NoMobile";
+import scroll from "./components/animations/scroll";
+import { useEffect } from "react";
 
 const App = () => {
+  useEffect(() => {
+    scroll();
+  });
   return (
     <main className="bg-gradient-to-r from-[#16161d] to-[#1f1f3a] min-h-screen overflow-hidden">
       <div className="hidden lg:block">
